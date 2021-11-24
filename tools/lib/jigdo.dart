be0ff96bedb170e21d1eefa8c044e5e6ab19be67
Kenneth Endfinger <kaendfinger@gmail.com>
2021-11-24 02:43:58 -0500

    Make jigdo STFU

--- tools/lib/jigdo.dart
+++ tools/lib/jigdo.dart
@@ -3,9 +3,10 @@ library boot.os.tools.jigdo;
 import 'dart:io';
 
 Future<void> runJigdoLiteIn(String path, List<String> args) async {
-  final process = await Process.start("jigdo-lite", args,
-      workingDirectory: path, mode: ProcessStartMode.inheritStdio);
-  if ((await process.exitCode) != 0) {
-    throw Exception("jigdo-lite ${args} failed in ${path}");
+  final process =
+      await Process.start("jigdo-lite", args, workingDirectory: path);
+  final exitCode = await process.exitCode;
+  if (exitCode != 0) {
+    throw Exception("jigdo-lite $args failed in $path: exit code = $exitCode");
   }
 }
